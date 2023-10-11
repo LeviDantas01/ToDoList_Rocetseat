cf285fcc111b34d9074d1c48283c7bbd0f261ea7
Levi <dantaslevi01@gmail.com>
2023-10-11 16:01:04 -0300

    frist commit

---
+++ todolist/src/test/java/br/com/levi/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.levi.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
